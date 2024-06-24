44c156a2d7ca98f05c00148b6a71062b9f789319
wangxuancheng <wangxuancheng123@gmail.com>
2024-06-24 13:40:07 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\ProcessUsers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -11,8 +10,6 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
     public function index(){
-        $user = User::find(1);
-        $amount=100;
-        ProcessUsers::dispatch($user, $amount);
+       
     }
 }
